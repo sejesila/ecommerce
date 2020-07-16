687811a38bc3f1e033ff17ca379341e2da97496d
silaseje <silasseje170100@daystar.ac.ke>
2020-07-16 07:33:19 +0300

    guest user cart

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,4 +1,3 @@
-// console.log('Hello world')
 var updateBtns = document.getElementsByClassName('update-cart')
 for (var i = 0; i < updateBtns.length; i++)
 {
@@ -7,11 +6,12 @@ for (var i = 0; i < updateBtns.length; i++)
     {
         var productId = this.dataset.product
         var action = this.dataset.action
-        console.log('prodctId:', productId, 'action:', action)
+        console.log('productId:', productId, 'action:', action)
         
         console.log('USER:', user)
-        if (user === 'AnonymousUser') {
-            console.log('Not logged in')
+            if (user == 'AnonymousUser') {
+            addCookieItem(productId,action)
+            
         } else
         {
             // console.log('User is logged in,sending data')
@@ -19,6 +19,31 @@ for (var i = 0; i < updateBtns.length; i++)
 
         }
     })
+}
+function addCookieItem(productId,action)
+{
+    console.log('Not logged in.... ')
+
+    if (action == 'add') {
+        if(cart[productId] == undefined){
+            cart[productId] = { 'quantity':1 }
+            
+        } else {
+            cart[productId]['quantity'] += 1
+        }
+    }
+    if (action == 'remove') {
+        cart[productId]['quantity'] -= 1
+
+        if (cart[productId]['quantity'] <= 0) {
+            console.log('Remove Item')
+            delete cart[productId]
+        }
+    }
+    console.log('Cart:', cart) 
+    document.cookie = 'cart=' + JSON.stringify(cart) +  ";domain=;path=/"
+    
+    // location.reload()
 }
 
 function updateUserOrder(productId, action)
@@ -45,3 +70,4 @@ function updateUserOrder(productId, action)
             location.reload()
         })
 }
+
--- store/views.py
+++ store/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
+import datetime
 from .models import *
+from .utils import cookieCart
+
 
 
 # Create your views here.
@@ -12,24 +15,27 @@ def store(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
-        items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0,'shipping':False}
-        cartItems = order['get_cart_items']
+        cookieData = cookieCart(request)
+        items = cookieData['items']
+        order = cookieData['order']                            #{'get_cart_total': 0, 'get_cart_items': 0,'shipping':False}
+        cartItems = cookieData['cartItems']              #order['get_cart_items']
         
     products = Product.objects.all()
     context = {'products':products,'cartItems': cartItems}
     return render(request, 'store/store.html', context)
     
 def cart(request):
+
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
-        items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0,'shipping':False}
-        cartItems = order['get_cart_items']
+        cookieData = cookieCart(request)
+        cartItems = cookieData['cartItems']
+        order = cookieData['order']
+        items = cookieData['items']
     context = {'items' : items, 'order': order,'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
 
@@ -40,10 +46,10 @@ def checkout(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
-        items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0,'shipping':False}
-        cartItems = order['get_cart_items']
-        
+        cookieData = cookieCart(request)
+        cartItems = cookieData['cartItems']
+        order = cookieData['order']
+        items = cookieData['items']
     context = {'items' : items, 'order': order,'cartItems': cartItems}
    
     return render(request, 'store/checkout.html', context)
@@ -72,5 +78,35 @@ def updateItem(request):
 
 
 def processOrder(request):
-    print('Data', request.body)
+    # print('Data', request.body)
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+    
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+            
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+
+                customer = customer,
+                order= order,
+                address = data['shipping']['address'],
+                city= data['shipping']['city'],
+                state = data['shipping']['state'],
+                zipcode = data['shipping']['zipcode'],
+                
+
+
+            )  
+
+    else:
+        print('User is not logged in..')    
     return JsonResponse('Payment Complete', safe=False)
